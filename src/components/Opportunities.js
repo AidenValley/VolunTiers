b2e72c571657a473e88af4c430823dacfe8934a5
cmomie <cmomie1@gmail.com>
2022-11-01 21:09:15 -0500

    opportunities component

--- src/components/Opportunities.js
+++ src/components/Opportunities.js
@@ -5,7 +5,7 @@ const Opportunities = () => {
         <div>
             <h1>Opportunities</h1>
             <p>This is Opportunities us...</p>
-            <p>I will add the volunteer cards here. 
+            <p>I will add the volunteer cards here. THe cards will go here 
             </p>
         </div>
     )
