25e75571ec915795207b7e6bedfa1ee8c68d0ddb
cmomie <cmomie1@gmail.com>
2022-11-05 15:06:57 -0500

    OppDetail Component 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Imports
 import React, { useEffect, useState } from 'react';
-import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
+import { NavLink, Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 
@@ -15,16 +15,26 @@ import Login from './components/Login';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 import Welcome from './components/Welcome';
-
-
 import Adminprofile from './components/Adminprofile';
 import Contact from './components/Contact';
 import Past from './components/Past';
 import OrganizationsContainer from './components/OrganizationsContainer';
 import Opportunities from './components/Opportunities';
-import OppDetail from './components/OppDetail';
 
+import Contact from './components/Contact';
+import Past from './components/Past';
+import OrganizationsContainer from './components/OrganizationsContainer';
+import OrgDetail from './components/OrgDetail';
 
+import Contact from './components/Contact';
+import Past from './components/Past';
+import OrganizationsContainer from './components/OrganizationsContainer';
+import DashboardNavbar from './components/DashboardNavbar';
+import OrgDetail from './components/OrgDetail';
+
+import Opportunities from './components/Opportunities';
+import Userprofile from './components/Userprofile';
+import Board from './components/Board';
 
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
@@ -32,9 +42,11 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
   let token = localStorage.getItem('jwtToken');
   console.log('===> Hitting a Private Route');
   return <Route {...rest} render={(props) => {
-    return token ? <Component {...rest} {...props} /> : <Redirect to="/login" />
+    return token ? <Component {...rest} {...props} /> : <Redirect to="/login"  />
   }} />
 }
+
+// Opportunities
 
 // Opportunities
 
@@ -75,6 +87,7 @@ function App() {
   return (
     <Router>
 
+
     <div className="App">
       <Navbar handleLogout={handleLogout} isAuth={isAuthenticated} />
       <div className="container mt-5">
@@ -85,7 +98,7 @@ function App() {
             render={(props) => <Login {...props} nowCurrentUser={nowCurrentUser} setIsAuthenticated={setIsAuthenticated} user={currentUser}/>}
           />
           <PrivateRoute path="/profile" component={Profile} user={currentUser} handleLogout={handleLogout} />
-          <PrivateRoute path="/adminprofile" component={Adminprofile} />
+          
           
           <Route exact path="/" component={Welcome} />
           <Route path="/about" component={About} />
@@ -94,13 +107,12 @@ function App() {
 
 
           <Route path="/opportunities" component={Opportunities} />
-          <Route path="/oppdetail" component={OppDetail} />
-
           <Route path="/organizations" component={OrganizationsContainer} />
-         
+          <Route path="/dashboardnavbar" component={DashboardNavbar} />
+          <Route path="/userprofile" component={Userprofile} />
+          <Route path="/orgdetail/:id" component={OrgDetail} />
+          <Route path="/board" component={Board} />
 
-
-       
         </Switch>
         
         </div>
