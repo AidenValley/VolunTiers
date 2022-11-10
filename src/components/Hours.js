fbf58c6bb290ec0253cab9aa0ef1fc61498007ce
AidenValley <jhyjsh953@gmail.com>
2022-11-09 19:47:27 -0800

    hour axios call edit

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,55 +1,54 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
-const  REACT_APP_SERVER_URL  = process.env.REACT_APP_SERVER_URL;
-
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function Hours(need, user) {
-
-  const [checkIn, setCheckIn] = useState('');
-  const [checkOut, setCheckOut] = useState('');
-  console.log(`###########>>>>>>`, need)
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  console.log(`###########>>>>>>`, need);
 
   const handleTimeIn = (e) => {
     e.preventDefault();
-    let now = new Date()
+    let now = new Date();
     setCheckIn(now);
-    console.log('%%%%%%%%%', checkIn)
-    };
+    console.log("%%%%%%%%%", checkIn);
+  };
 
   const handleTimeOut = (e) => {
     e.preventDefault();
     setCheckOut(new Date());
-    console.log(checkOut)
+    console.log(checkOut);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post(`${REACT_APP_SERVER_URL}/hours/submitTime/6365e304bcb8c11fcd5e60a9`, { 
-        email: user.email,
-        signIn: checkIn,
-        signOut: checkOut
-     })
-        //need to push userid into users array in Opportunity 
-        .then(response => {
-            console.log('===> Yay, update was made');
-            console.log(response);
-        })
-        .catch(error => console.log('===> Error in Submission', error));
-}
+    axios
+      .post(
+        `${REACT_APP_SERVER_URL}/hours/submitTime/${need.opportunities._id}`,
+        {
+          email: user.email,
+          signIn: checkIn,
+          signOut: checkOut,
+        }
+      )
+      //need to push userid into users array in Opportunity
+      .then((response) => {
+        console.log("===> Yay, update was made");
+        console.log(response);
+      })
+      .catch((error) => console.log("===> Error in Submission", error));
+  };
   return (
     <div>
-        <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <button onClick={handleTimeIn}>Check In</button>
-      <button onClick={handleTimeOut}>Check Out</button>
+        <button onClick={handleTimeOut}>Check Out</button>
 
-      <button type="submit" className="btn btn-primary float-right">Submit</button>
+        <button type="submit">Submit</button>
       </form>
-
     </div>
   );
 }
-
-
 
 export default Hours;
