cb43df0dfd490de7782dd1249efdcd414d7440f4
AidenValley <jhyjsh953@gmail.com>
2022-11-09 19:25:14 -0800

    about save

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            <h2>Make a Difference in your Community</h2>
-            <h1>VolunTier</h1>
-            <p> Do good, reach new levels: </p>
-            <p> VolunGold </p>
-            <p> VolunSilver </p>
-            <p> VolunBronze </p>
+  return (
+    <div>
+      <h1 class="title">Make a Difference in your Community</h1>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <h3> VolunGold </h3>
+            <span class="fa fa-star checked"></span>
+            <p>VolunTiers With 40 Hours or More</p>
+          </div>
         </div>
-    )
-}
+        <div class="column">
+          <div class="card">
+            <h3> VolunSilver </h3>
+            <span class="fa fa-star silver"></span>
+            <p>VolunTiers With 30 Hours or More</p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <h3>VolunBronze</h3>
+            <span class="fa fa-star bronze"></span>
+            <p>VolunTiers With 20 Hours or More</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
