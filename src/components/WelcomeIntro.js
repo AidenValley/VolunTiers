0cb231364c06ae1627e3d11d846f95140fed99e2
AidenValley <jhyjsh953@gmail.com>
2022-11-09 18:59:55 -0800

    welcome, intro save

--- src/components/WelcomeIntro.js
+++ src/components/WelcomeIntro.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { MDBContainer } from 'mdb-react-ui-kit';
+import {MDBCardTitle } from 'mdb-react-ui-kit';
 
 const WelcomeIntro = () => {
     return (
       <>
-      <MDBContainer fluid>Helloooo</MDBContainer>
+      <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold text-center" style={{}}>
+        VolunTier: Make a Difference in your Community
+      </MDBCardTitle>
       </>
     )
 }
