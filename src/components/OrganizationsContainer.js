01e4b63a662ed48f44108e8441f323d84e03751e
AidenValley <jhyjsh953@gmail.com>
2022-11-26 13:47:34 -0800

    organization container fix

--- src/components/OrganizationsContainer.js
+++ src/components/OrganizationsContainer.js
@@ -1,83 +1,104 @@
-// Imports
-import React, { useState } from 'react';
-import axios from 'axios';
-import OppCreateForm from './OppCreateForm';
-import logo from '../images/VolunTierLogo.png';
-import {
-    MDBContainer,
-    MDBInput,
-    MDBCardTitle
-  }
-  from 'mdb-react-ui-kit';
+import React, { useState } from "react";
+import axios from "axios";
+import OppCreateForm from "./OppCreateForm";
+import logo from "../images/VolunTierLogo.png";
+import { MDBContainer, MDBCardTitle } from "mdb-react-ui-kit";
 
 const { REACT_APP_SERVER_URL } = process.env;
 
 const OrganizationsContainer = () => {
-    const [org, setOrg] = useState({
-        orgName: "",
-        contactPerson: "",
-        contactEmail: "",
-        contactPhone: ""
-    });
-
-
-    const [created, setCreated] = useState(false)
-
-    const handleChange = (e) => {
-        setOrg({ ...org, [e.target.name]: e.target.value });
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newOrganization = org;
-        axios.post(`${REACT_APP_SERVER_URL}/organizations/`, newOrganization)
-            .then(response => {
-                console.log('===> Yay, new organization');
-                console.log(response);
-                setCreated(true);
-                setOrg(response.data.organizations)
-            })
-    }
-
-    if (created) {
-
-        return (<div>
-            <h1>Welcome, {org.orgName}!</h1>
-            <p>You can create an event or look at who's registered for your event here.</p>
-            <div>
-                <OppCreateForm org={org._id} />
-            </div>
-        </div>)
-    }
-
-
+  const [org, setOrg] = useState({
+    orgName: "",
+    contactPerson: "",
+    contactEmail: "",
+    contactPhone: "",
+  });
+  const [created, setCreated] = useState(false);
+  const handleChange = (e) => {
+    setOrg({ ...org, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newOrganization = org;
+    axios
+      .post(`${REACT_APP_SERVER_URL}/organizations/`, newOrganization)
+      .then((response) => {
+        console.log("===> Yay, new organization");
+        console.log(response);
+        setCreated(true);
+        setOrg(response.data.organizations);
+      });
+  };
+  if (created) {
     return (
-        
-        
-        <form onSubmit={handleSubmit}>
-        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-            <div className="text-center">
-                <img src={logo} alt="logo"/>
-            </div>
-            <MDBCardTitle className='fa-h5 fa-solid h1-responsive pt-3 m-5 text-center'>
-                <h3 className=''> Please Register Your Organization</h3>
-            </MDBCardTitle>
-            <br />
-            
-            <MDBInput wrapperClass='mb-4' placeholder='Organization Name' id='formControlLg' type='name' size="lg" value={org.orgName} onChange={handleChange}/>
-            <MDBInput wrapperClass='mb-4' placeholder='Email Address' id='formControlLg' type='email' size="lg" value={org.contactEmail} onChange={handleChange}/>
-            <MDBInput wrapperClass='mb-4' placeholder='Contact Name' id='formControlLg' type='password' size="lg" value={org.contactPerson} onChange={handleChange}/>
-            <MDBInput wrapperClass='mb-4' placeholder='Phone' id='formControlLg' type='password' size="lg" value={org.contactPhone} onChange={handleChange}/>
-            <div className="d-flex justify-content-between mx-3 mb-4">
-            
-                
-            </div>
-        
+      <div>
+        <h1>Welcome, {org.orgName}!</h1>
+        <p>
+          You can create an event or look at who's registered for your event
+          here.
+        </p>
+        <div>
+          <OppCreateForm org={org._id} />
+        </div>
+      </div>
+    );
+  }
 
-            <button className="btn btn-success" size='lg'>Submit</button>
-        </MDBContainer>
-    </form>
-    )
-}
+  return (
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+      <div className="text-center">
+        <img src={logo} alt="logo" />
+      </div>
+      <MDBCardTitle className="fa-h5 fa-solid h1-responsive pt-3 m-5 text-center">
+        <h3 className=""> Please Register Your Organization</h3>
+      </MDBCardTitle>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group mb-4">
+          <input
+            type="text"
+            name="orgName"
+            placeholder="Organization Name"
+            value={org.orgName}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group mb-4">
+          <input
+            type="email"
+            name="contactEmail"
+            placeholder="Email Address"
+            value={org.contactEmail}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group mb-4">
+          <input
+            type="text"
+            name="contactPerson"
+            placeholder="Contact Name"
+            value={org.contactPerson}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group mb-4">
+          <input
+            type="text"
+            name="contactPhone"
+            placeholder="Phone Number"
+            value={org.contactPhone}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
 
+        <button type="submit" size="lg" className="btn btn-success">
+          Submit
+        </button>
+      </form>
+    </MDBContainer>
+  );
+};
 export default OrganizationsContainer;
