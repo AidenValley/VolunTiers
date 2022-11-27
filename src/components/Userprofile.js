4afd8469aa2ad77f0d2574ef90a9a6bf596f2f3e
AidenValley <jhyjsh953@gmail.com>
2022-11-27 01:23:07 -0800

    userprofile styling readjust

--- src/components/Userprofile.js
+++ src/components/Userprofile.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import setAuthToken from "../utils/setAuthToken";
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
 import axios from "axios";
 const { REACT_APP_SERVER_URL } = process.env;
 
@@ -21,59 +21,51 @@ const Userprofile = () => {
   };
 
   return (
-    <section className="vh-100" style={{ backgroundColor: '#f4f5f7' }}>
-    <MDBContainer className="py-5 h-100">
-      <MDBRow className="justify-content-center align-items-center h-100">
-        <MDBCol lg="6" className="mb-4 mb-lg-0">
-          <MDBCard className="mb-3" style={{ borderRadius: '.5rem' }}>
-            <MDBRow className="g-0">
-              <MDBCol md="4" className="gradient-custom text-center text-white"
-                style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
-                <MDBCardImage src="https://www.cityheadshots.com/uploads/5/1/2/1/5121840/highres-mjb-4556_orig.jpg"
-                  alt="Avatar" className="my-5" style={{ width: '80px' }} fluid 
-                  width="320"
-                  height="250" 
-                  />
-                <MDBTypography tag="h5"></MDBTypography>
-                <MDBIcon far icon="edit mb-5" />
-              </MDBCol>
-              <MDBCol md="8">
-                <MDBCardBody className="p-4">
-                  <MDBTypography tag="h6">{profile.name}
-                  </MDBTypography>
-                  <hr className="mt-0 mb-4" />
-                  <MDBRow className="pt-1">
-                    <MDBCol size="10" className="mb-3">
-                      <MDBTypography tag="h6">Email</MDBTypography>
-                      <MDBCardText className="text-muted">{profile.email}</MDBCardText>
-                    </MDBCol>
-                    <MDBCol size="10" className="mb-3">
-                      <MDBTypography tag="h6">Phone</MDBTypography>
-                      <MDBCardText className="text-muted">{profile.phone}</MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
+    <div className="vh-100" style={{ backgroundColor: '#9de2ff' }}>
+      <MDBContainer>
+        <MDBRow className="justify-content-center">
+          <MDBCol md="9" lg="5" xl="5" className="mt-5">
+            <MDBCard style={{ borderRadius: '15px' }}>
+              <MDBCardBody className="p-4">
+                <div className="text-black">
+                  <div className="flex-">
+                    <MDBCardImage
+                      style={{ width: '180px', borderRadius: '10px' }}
+                      src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC5ww3E4QY6wKDfYAMfRH0AFf601HUU9HLNQ&usqp=CAU'
+                      alt='Generic placeholder image'
+                      fluid />
+                  </div>
+                  <div className="flex-grow-1 ms-3">
+                    <MDBCardTitle>{profile.name}</MDBCardTitle>
+                    <MDBCardText>VolunTier Bronze Member, {profile.role}</MDBCardText>
 
-                  <MDBTypography tag="h6">Your Contribution</MDBTypography>
-                  <hr className="mt-0 mb-4" />
-                  <MDBRow className="pt-1">
-                    <MDBCol size="6" className="mb-3">
-                      <MDBTypography tag="h6">Your Role</MDBTypography>
-                      <MDBCardText className="text-muted">{profile.role}</MDBCardText>
-                    </MDBCol>
-                    <MDBCol size="6" className="mb-3">
-                      <MDBTypography tag="h6">Total Service Hours</MDBTypography>
-                      <MDBCardText className="text-muted">{profile.hours}</MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
-
-                </MDBCardBody>
-              </MDBCol>
-            </MDBRow>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  </section>
+                    <div className="d-flex justify-content-start rounded-3 p-2 mb-2"
+                      style={{ backgroundColor: '#efefef' }}>
+                      <div>
+                        <p className="small text-muted mb-1">Email</p>
+                        <p className="mb-0">{profile.email}</p>
+                      </div>
+                      <div className="px-4">
+                        <p className="small text-muted mb-1">Phone</p>
+                        <p className="mb-0">{profile.phone}</p>
+                      </div>
+                      <div>
+                        <p className="small text-muted mb-1">Total Service Hours</p>
+                        <p className="mb-0">{profile.hours}</p>
+                      </div>
+                    </div>
+                    <div className="">
+                      <MDBBtn className="btn btn-success">Create Event</MDBBtn>
+                      <MDBBtn className="flex-grow-1">Back to Opportunities</MDBBtn>
+                    </div>
+                  </div>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
   )};
 
 export default Userprofile;
