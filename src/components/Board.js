a4db118817c0c28c0ad14eaa1597040d0454fe73
AidenValley <jhyjsh953@gmail.com>
2022-11-28 21:38:42 -0800

    Board styling add

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,41 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  MDBCard,
+  MDBListGroup,
+  MDBListGroupItem,
+  MDBCardTitle,
+} from "mdb-react-ui-kit";
+import logo from "../images/VolunTierLogo.png";
 const { REACT_APP_SERVER_URL } = process.env;
 
 const Board = () => {
-    const [leaders, setLeaders] = useState([]);
+  const [leaders, setLeaders] = useState([]);
 
-    useEffect(() => {
-        fetchLeaders();
-    }, [])
-    useEffect(() => {
-        console.log(leaders);
+  useEffect(() => {
+    fetchLeaders();
+  }, []);
+  useEffect(() => {
+    console.log(leaders);
+  }, [leaders]);
 
-    }, [leaders])
+  const fetchLeaders = async () => {
+    const response = await axios.get(`${REACT_APP_SERVER_URL}/users/leaders`);
+    setLeaders(response.data);
+  };
 
-    const fetchLeaders = async () => {
-        const response = await axios.get(`${REACT_APP_SERVER_URL}/users/leaders`)
-        setLeaders(response.data);
-    }
-
-
-    return (
-        <div className="board">
-            <h1 className='leaderboard'>Top Tier Volunteers</h1>
-
-            <div style={{ alignItems: 'center', margin: '20px 60px' }}>
-                <ol>
-                    {
-                        leaders.map((leader) => {
-                            return <li key={leader.id}>{ leader.name } - {leader.hours } hours</li>
-                        })
-                    }
+  return (
+    <div className="board">
+      <h1 className="leaderboard text-center">
+         Top Tier Volunteers{" "}
+      </h1>
+      <MDBCard>
+        <MDBListGroup flush>
+          <MDBListGroupItem>
+            {leaders.map((leader) => {
+              return (
+                <ol key={leader.id}>
+                  <span class="fa fa-star checked"></span><strong>{leader.name} </strong> - {leader.hours} hours
                 </ol>
-            </div>
-
-
-        </div>
-    );
-}
+              );
+            })}
+          </MDBListGroupItem>
+        </MDBListGroup>
+      </MDBCard>
+    </div>
+  );
+};
 
 export default Board;
